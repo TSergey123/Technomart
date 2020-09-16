07e5070ec4106bff28d2a683f9db17c1db60bb98
toporkov-sv <toporkov123561@mail.ru>
2020-09-16 18:40:50 +0300

    Min and max price range

--- js/script.js
+++ js/script.js
@@ -5,15 +5,25 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
-  modal.style.display = "block";
+	modal.style.display = "block";
 };
 
 span.onclick = function () {
-  modal.style.display = "none";
+	modal.style.display = "none";
 };
 
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 };
+
+
+function minRangeOut(val) {
+    document.getElementById("min_price_input").value = val;
+}
+
+function maxRangeOut(val) {
+    document.getElementById("max_price_input").value = val;
+
+}   
